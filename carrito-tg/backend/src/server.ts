c29317fed0aee014906a8952b8034d88f671a87b
Jason Madrid <jasonhnn6@gmail.com>
2026-05-15 20:07:42 -0600

    Creando estructura backend

---
+++ carrito-tg/backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import "./config/db";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
